d29ee96259aad932d21ed3c0543db90df8bc4a3a
jabss <jabrijuhinin93@gmail.com>
2024-09-28 21:08:31 +0800

    grid now displaying 9 squares. perfect !

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,36 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Squares() {
+  let square = [];
+
+    for (let i=0; i<9; i++) {
+      square.push(<div 
+                key={i}
+                style={{
+                height: '60px',
+                width: '60px',
+                display: 'inline-block',
+                border: 'solid black 2px'
+              }}></div>
+    )}
+
+    return (
+      <div 
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(3, 60px)'
+        }}>{square}</div>
+    )
+}
+
 function App() {
   const [count, setCount] = useState(0)
   console.log("INSPECT button", count)
 
   return (
     <div>
-     
+      <Squares />
 
       <p> Hello, world !</p>
       
