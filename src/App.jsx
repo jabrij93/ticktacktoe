4488b888d95786a4e9fd803d03b4de862fe71096
jabss <jabrijuhinin93@gmail.com>
2024-09-28 12:22:18 +0800

    1 square is displayed

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,32 @@ function App() {
   const [count, setCount] = useState(0)
   console.log("INSPECT button", count)
 
+  let square = []
+
   return (
     <div>
       <p> Hello, world !</p>
+
+      <div 
+        style={{ 
+          height: '50px',
+          width: '50px',
+          border: 'solid black 2px',
+          color: 'grey',
+          display: 'inline-block',
+        }}
+      > 
+    
+        {square.push[
+          <div 
+            style={{
+            display:'grid',
+            gridTemplateColumns: 'repeat(3, 50px)',
+          }}>
+          </div>
+        ]}
+        
+      </div>
       
       <button onClick={()=>setCount(count + 1)}>CLICK ME</button>
       <p> COUNT : {count} </p>
