59c9f09072d80058a21c37a5749118f6f899e68d
jabss <jabrijuhinin93@gmail.com>
2024-09-29 19:12:57 +0800

    working again

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,39 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Squares() {
+  let squares = []
+
+  for (let i=0; i<9; i++) {
+    squares.push(
+      <div style={{
+        height:'50px',
+        width:'50px',
+        style:'inline-block',
+        border:'solid black 2px',
+      }}/>
+    )
+  }
+
+  return (
+    <div
+      style={{
+        height: '50px',
+        width: '50px',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3, 50px)'
+      }}
+    > {squares}</div>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
   console.log("INSPECT button", count)
 
   return (
     <div>
-  
+      <Squares />  
 
       <p> Hello, world !</p>
       
